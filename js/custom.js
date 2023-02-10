eed17d09805bedd2eeb3901c28c0b3ecf248d6d9
sumitwebteasor <sumit.webteasor@gmail.com>
2023-02-10 17:15:38 +0530

    organise hiring event added

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,41 @@ $(window).scroll(function(){
   if (scroll >= 100) sticky.addClass('fixed');
   else sticky.removeClass('fixed');
 });
+
+$(document).ready(function (){
+	$("#prev-vid-btn").on("click", function (){
+		let vid = $("#prev-vid")[0];
+		if (vid.paused) {
+            vid.play();
+			$("#prev-vid-btn img").attr("src", "images/pause.png");
+        } else {
+            vid.pause();
+			$("#prev-vid-btn img").attr("src", "images/play.png");
+        }
+	});
+});
+
+$('.testimonial-slider').owlCarousel({
+                loop: true, 
+                margin: 10,
+                autoplay: true,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: true
+                    }, 
+                    600: {
+                        items: 2,
+                        nav: false
+                    },
+                    1000: {
+                        items: 3,
+                        nav: true,
+                        loop: true
+                    }
+                }
+            })
+			
+			
+			
