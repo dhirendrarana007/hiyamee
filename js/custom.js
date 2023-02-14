dc26b15470390f14f9a4c6ce250d2aef2e45bff8
sumitwebteasor <sumit.webteasor@gmail.com>
2023-02-14 17:22:09 +0530

    Dashboard added

--- js/custom.js
+++ js/custom.js
@@ -26,31 +26,84 @@ $(document).ready(function (){
 });
 
 $('.testimonial-slider').owlCarousel({
-                loop: true, 
-                margin: 10,
-                autoplay: true,
-                responsiveClass: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: true
-                    }, 
-                    600: {
-                        items: 2,
-                        nav: false
-                    },
-                    1000: {
-                        items: 2,
-                        nav: true,
-                        loop: true
-                    },
-					1200: {
-                        items: 3,
-                        nav: true,
-                        loop: true
-                    }
-                }
-            })
+	loop: true, 
+	margin: 10,
+	autoplay: true,
+	responsiveClass: true,
+	responsive: {
+		0: {
+			items: 1,
+			nav: true
+		}, 
+		600: {
+			items: 2,
+			nav: false
+		},
+		1000: {
+			items: 2,
+			nav: true,
+			loop: true
+		},
+		1200: {
+			items: 3,
+			nav: true,
+			loop: true
+		}
+	}
+})
 			
 			
-			
+$('.video-slider').owlCarousel({
+	loop: true, 
+	margin: 40,
+	autoplay: true,
+	responsiveClass: true,
+	responsive: {
+		0: {
+			items: 1,
+			nav: true
+		}, 
+		600: {
+			items: 2,
+			nav: false
+		},
+		1000: {
+			items: 2,
+			nav: true,
+			loop: true
+		},
+		1200: {
+			items: 2,
+			nav: true,
+			loop: true
+		}
+	}
+})
+
+$('.video-sliders').owlCarousel({
+	loop: true, 
+	margin: 40,
+	autoplay: true,
+	responsiveClass: true,
+	responsive: {
+		0: {
+			items: 1,
+			nav: true
+		}, 
+		600: {
+			items: 2,
+			nav: false
+		},
+		1000: {
+			items: 4,
+			nav: true,
+			loop: true
+		},
+		1200: {
+			items: 5,
+			nav: true,
+			loop: true
+		}
+	}
+})
+
